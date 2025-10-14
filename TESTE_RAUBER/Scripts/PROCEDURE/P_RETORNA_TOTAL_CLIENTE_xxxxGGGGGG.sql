73e873757f0ce2cd1fca5ad0f64cbb5b523d9771
RobotFileGenerator <robot@system.local>
2025-10-14 01:59:11 -0300

    Automated commit

---
+++ TESTE_RAUBER/Scripts/PROCEDURE/P_RETORNA_TOTAL_CLIENTE_xxxxGGGGGG.sql
@@ -0,0 +1,8 @@
+create OR ALTER procedure [dbo].[P_RETORNA_TOTAL_CLIENTE_xxxxGGGGGG]
+as 
+begin 
+	select 
+		COUNT(1) AS 'Total Clientes'
+	,	MAX(DATA_HORA) AS 'ultimo clienteXXXXXXXXX'
+	from CLIENTE
+end
