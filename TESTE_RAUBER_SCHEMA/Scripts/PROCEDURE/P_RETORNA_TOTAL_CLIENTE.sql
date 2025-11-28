579c5acb0e2ff9e2798ffdbd39e45a3c11bc3779
RobotFileGenerator <robot@system.local>
2025-11-28 16:19:55 -0300

    Automated commit

--- TESTE_RAUBER_SCHEMA/Scripts/PROCEDURE/P_RETORNA_TOTAL_CLIENTE.sql
+++ TESTE_RAUBER_SCHEMA/Scripts/PROCEDURE/P_RETORNA_TOTAL_CLIENTE.sql
@@ -1,12 +1,10 @@
-
-
 ALTER   procedure [dbo].[P_RETORNA_TOTAL_CLIENTE]
 as 
 begin 
 	select 
 		'TESTE' AS 'COLUNA NOVA'
-	,	COUNT(1) AS 'Total Clientes'
-	,	MAX(DATA_HORA) AS 'ultimo Cadastro Realizado'
 	,	MIN(DATA_HORA) AS 'PRIMEIRO REGISTRO'		
+	,	COUNT(1) AS 'Total Clientes'
+	,	MAX(DATA_HORA) AS 'ultimo Cadastro Realizado'	
 	from CLIENTE
 end
